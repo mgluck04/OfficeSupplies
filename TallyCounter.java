3691f247ca973b3883f5df436dc0a8c1827e6682
Max C. Gluck <maxgluck@Maxs-MacBook.local>
2022-01-26 13:43:41 -0800

    Finished TallyCounter class

--- TallyCounter.java
+++ TallyCounter.java
@@ -1,5 +1,19 @@
 package OfficeSupplies;
 
 public class TallyCounter {
-    
+    //fields
+    private int counter;
+
+    //constructor
+    public TallyCounter()
+    {
+        counter = 0;
+    }
+
+    //methods
+    public int counterAdd()
+    {
+        counter++;
+        return counter;
+    }
 }
